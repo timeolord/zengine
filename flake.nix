e56a9361cb263bc1d16a9c638340ac052e5205a7
timeolord <timeolord6677@gmail.com>
2026-04-10 17:43:45 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "zig-sti";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            zig
+            zls
+            # xorg-server
+            # libxrandr
+            # libxcursor
+            # libxfixes
+            # libxi
+            # libxinerama
+            # libxrender
+            valgrind
+          ];
+        };
+      }
+    );
+}
